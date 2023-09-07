7e7f8a7a6cbacf9ddc375e385f3ae933a3d0f438
Ionut Mnere <m.ionut.andrei@gmail.com>
2023-09-07 23:35:06 +0300

    Created first model

--- www/api/models.py
+++ www/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+SHORT_TEXT = 16
+
+
+class VoltageSample(models.Model):
+	cell_id = models.CharField(max_length=SHORT_TEXT)
+	voltage = models.DecimalField(decimal_places=2, max_digits=4)
+	timestamp = models.DateTimeField(auto_now=True)
